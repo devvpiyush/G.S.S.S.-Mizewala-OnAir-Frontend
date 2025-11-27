8f528152fa9b90ef8bd1d6086bdabf980a3bbea2
GSSS-Mirzewala <gsssmirzewala1954@gmail.com>
2025-11-27 16:37:33 +0530

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function API(REQUEST_TYPE, END_POINT, CREDENTIALS, DATA) {
-  const API_URI = `http://api.gsssmirzewala.in/api/${END_POINT}`;
+  const API_URI = `https://api.gsssmirzewala.in/api/${END_POINT}`;
 
   if (REQUEST_TYPE === "GET") {
     return await axios.get(API_URI, {
