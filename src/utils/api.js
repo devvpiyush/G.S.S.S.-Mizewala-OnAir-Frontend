ae8ab7894764f8eba25e652e3a043eb6b4ccfa25
GSSS-Mirzewala <gsssmirzewala1954@gmail.com>
2026-01-12 17:45:14 +0530

    SMALL FIX

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 async function api(REQUEST_TYPE, END_POINT, WITH_CREDENTIALS = true, data) {
   try {
-    const API_URI = `http://localhost:8000/api/${END_POINT}`;
+    const API_URI = `${import.meta.env.JWT_SECRET}/api/${END_POINT}`;
     if (REQUEST_TYPE === "GET") {
       return await axios.get(API_URI, {
         withCredentials: WITH_CREDENTIALS ? true : false,
