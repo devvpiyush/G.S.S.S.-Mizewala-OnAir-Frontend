728063e64ff746c11d4c7bcb4c0dd7fa5729a765
GSSS-Mirzewala <gsssmirzewala1954@gmail.com>
2026-02-15 23:10:32 +0530

    Update Categories.jsx

--- src/pages/Music/Categories.jsx
+++ src/pages/Music/Categories.jsx
@@ -1,6 +1,6 @@
 function Categories({ list, selected, changeCategory }) {
   return (
-    <div className="w-full py-2 px-6 flex items-center justify-center gap-8">
+    <div className="w-full py-2 px-10 sm:px-6 flex items-center justify-center gap-8 overflow-x-auto">
       <button
         className={`w-fit px-6 py-1 ${selected === "All" ? "bg-black text-white" : "bg-white text-black"} rounded-lg font-medium cursor-pointer border border-[#c0c0c0]`}
         style={{ fontFamily: "Inter, sans-serif" }}
