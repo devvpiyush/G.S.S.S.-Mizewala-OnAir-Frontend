1cfa7d2e090057a18a4163fa80cc39a7ae3dbcc2
= <gsssmirzewala1954@gmail.com>
2026-02-16 21:39:28 +0530

    Update Protectors.jsx

--- src/contexts/Protectors.jsx
+++ src/contexts/Protectors.jsx
@@ -19,7 +19,7 @@ const Routes = [
   { path: "/release_notes", type: "public" },
   { path: "/notifications", type: "public" },
   { path: "/profile", type: "private" },
-  { path: "/music", type: "public" },
+  { path: "/music", type: "private", access: ["Teacher"] },
 ];
 
 export const BPS = ({ children }) => {
